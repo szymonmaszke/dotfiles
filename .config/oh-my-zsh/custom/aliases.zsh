d5aaa7148e9fdea3bde176a9f6b9f860f78545fe
vyzyv <vyz@protonmail.com>
2017-12-03 22:06:49 +0100

    Update essentials configs
    Former-commit-id: 287d81bf95ebb7af4cd175ae1b5a939756bfd225 [formerly 626489ad1e2141309dff68f1038a2b436bfd70de] [formerly bbf76ac5d1da9ad292aa7b6840dce4231228b6e9 [formerly 7b359d31bc1527b42ea3b1ea4758124b28f48bd1]]
    Former-commit-id: 80af2000418f881d366e570e2c9d3b77baa48e34 [formerly c855ef39dc34d0a84002a537c9b8b10e539e6307]
    Former-commit-id: 77867ec981139e19685fab743d26e5c39f5495ec

--- .config/oh-my-zsh/custom/aliases.zsh
+++ .config/oh-my-zsh/custom/aliases.zsh
@@ -1,29 +1,58 @@
 #main usage aliases
 alias c='clear'
 alias off='shutdown now'
-alias rest='shutdown now -r'
+alias restart='shutdown now -r'
 #program specific
 alias e='nvim'
 alias se='sudo -E nvim'
-alias net='sudo wifi-menu'
+alias net='sudo wifi-menu 2>/dev/null'
 alias firefox='firefox >/dev/null 2>/dev/null &'
-alias xmind='~/aur/xmind/XMind_amd64/XMind >/dev/null 2>/dev/null &'
+alias spotify='spotify >/dev/null 2>/dev/null &'
+alias g++='g++ -march=native -std=c++17 -O2 -Werror -Wextra -Wall -Weffc++ -Wnarrowing'
+alias py='python -b -B -O -W once -q '
+alias xmind='cd $HOME/aur/xmind/XMind_amd64 && (./XMind >/dev/null 2>&1 &) && cd - && clear'
+
+pdf(){
+  apvlv $1 >/dev/null 2>&1 &
+}
+
+jup(){
+  jupyter notebook $1 >/dev/null 2>&1 &
+}
+alias qjup='kill $(pgrep jupyter)'
 
 #pacman specific
-alias pup='sudo pacman -Syyu'
+alias pup='sudo pacman -Syyu --noconfirm && sudo gem update && sudo npm update -g && sudo pip-review --auto'
 alias pget='sudo pacman -S'
 alias prm='sudo pacman -Rsc'
 alias pls='sudo pacman -Qe'
 
+# https://github.com/athityakumar/colorls
+#colorls aliases <3
+alias l='colorls --sort-dirs'
+alias lf='colorls --files'
+alias ld='colorls --dirs'
+alias la='colorls --sort-dirs -a'
+
+alias ls='colorls --sort-dirs'
+
+#
 #ls aliases
-alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
-alias ll='ls -lh --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
-alias la='ls -lha --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
-alias l='ls'
+# alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+# alias ll='ls -lh --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+# alias la='ls -lha --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+# alias l='ls'
+
+#revert last commit
+alias grlc='git reset HEAD~'
+alias gp='git push -u origin $(git rev-parse --abbrev-ref HEAD)'
 
 #config aliases
 alias en='nvim ~/.config/nvim/init.vim'
 alias ea='nvim ~/.config/oh-my-zsh/custom/aliases.zsh'
+alias ez='nvim ~/.zshrc'
+alias et='nvim ~/.config/termite/config'
+alias ei='nvim ~/.config/i3/config'
 
 #docker specific
 #--format using regex, formatting of given output, check it, p.58
@@ -63,7 +92,7 @@ alias dp='sudo docker pull'
 alias di='sudo docker images'
 #removes given image
 alias drmi='sudo docker rmi'
-alias drmiall='sudo docker ps -a -q | xargs sudo docker rmi'
+alias drmiall='sudo docker rmi $(sudo docker images)'
 #given name searches images in registry
 alias dsearch='sudo docker search'
 
