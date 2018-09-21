8d8777499f73cb772fb81b2ac7c20e8af54b34b8
vyzyv <vyz@protonmail.com>
2018-09-21 10:16:48 +0200

    Cosmetic updates

--- polybar/scripts/bluetooth.sh
+++ polybar/scripts/bluetooth.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-inactive_color='#424242'
-active_color='#1793d1'
+inactive_color='#373737'
+active_color='#fabd2f'
 
 if [ "$(systemctl is-active bluetooth.service)" = "active" ]; then
-	echo "%{F${active_color}}%{F-}"
+	echo ""
 else
 	echo "%{F${inactive_color}}%{F-}"
 fi
--- polybar/scripts/dropdown/entrypoints/utils/internet_info/ip.sh
+++ polybar/scripts/dropdown/entrypoints/utils/internet_info/ip.sh
@@ -7,11 +7,3 @@ tput setaf 4
 printf "Public IP: "
 tput sgr0
 printf "%s\\n" "$public_ip"
-
-local_ip=$(ifconfig | rg 255.255.255 | awk '{ print $2 }')
-
-tput bold
-tput setaf 4
-printf "Local IP: "
-tput sgr0
-printf "%s\\n" "$local_ip"
