f34cd7afda3f90d5ff43f09d1ec25dbb76e9c424
vyzyv <vyz@protonmail.com>
2019-02-10 20:26:14 +0100

    Fix parsing of free disk space

--- polybar/scripts/root_system.sh
+++ polybar/scripts/root_system.sh
@@ -1,17 +1,15 @@
-occupied_space=$(df / | grep / | cut -f 8,8 -d\  | cut -f 1 -d%)
+occupied_space=$(df -h / | grep / | awk '{print $5}' | cut -f 1 -d%)
 
 fantastic_color='#27cc18'
 warning_color='#fabd2f'
 critical_color='#f44336'
 
 if [ "$occupied_space" -le 30 ]; then
-	echo "%{F${fantastic_color}}%{F-}"
-elif [ "$occupied_space" -le 50 ]; then
-	echo ""
-elif [ "$occupied_space" -le 70 ]; then
-	echo "%{F${warning_color}}%{F-}"
-elif [ "$occupied_space" -le 90 ]; then
-	echo "%{F${critical_color}}%{F-}"
-elif [ "$occupied_space" -le 100 ]; then
-	echo "%{F${critical_color}}%{F-}"
+  echo "%{F${fantastic_color}}%{F-}"
+elif [ "$occupied_space" -le 60 ]; then
+  echo ""
+elif [ "$occupied_space" -le 80 ]; then
+  echo "%{F${warning_color}}%{F-}"
+else
+  echo "%{F${critical_color}}%{F-}"
 fi
